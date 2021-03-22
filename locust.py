752e6a587db257b7060724991995508d318e8fa4
Linus Kendall <me@linuskendall.com>
2021-03-22 12:17:02 +0000

    Upodated test file
    Added a few expensive calls
    Added additional error checking

--- locust.py
+++ locust.py
@@ -21,9 +21,10 @@ mints = ["4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R"] #raydium
 # used for: getTokenAccountsByOwner
 token_owners = ["122FAHxVFQDQjzgSBSNUmLJXJxG4ooUwLdYvgf3ASs2K"] #token owner of raydium
 
-# A user of wallets
-class TrafficSimulator(HttpUser):
+# Base class for Solana 
+class SolanaUser(HttpUser):
   wait_time = between(0.1,1)
+  abstract = True
 
   def get_req_json(self, method, params=[]):
     req =  {"jsonrpc":"2.0","id":1,"method":method}
@@ -33,10 +34,112 @@ class TrafficSimulator(HttpUser):
 
   def rpc(self, method, params=[]):
    with self.client.post('/', data=self.get_req_json(method,params),  headers={'content-type': 'application/json'}, name=method, catch_response=True) as response:
+      try: 
+        json_data = response.json()
+        if "error" in json_data:
+          response.failure(json_data["error"]["message"])
+      except json.decoder.JSONDecodeError as e:
+        response.failure("Invalid json returned: "+response.text+" "+str(e))
+
+  def on_start(self):
+    # Randomly select values
+    self.stake_account = random.choice(stake_accounts)
+    self.wallet = random.choice(wallets)
+    self.wallets = random.sample(wallets,2)
+    self.confirmed_sigs = random.choice(confirmed_signature_address)
+    self.token_owner = random.choice(token_owners)
+    self.token_account = random.choice(token_accounts)
+    self.program_key = random.choice(program_keys)
+    self.transaction_signature = random.choice(transaction_signatures)
+    self.mint = random.choice(mints)
+   
+    # Get minimum stored ledger slot
+    with self.client.post('/', data=self.get_req_json("minimumLedgerSlot"),  headers={'content-type': 'application/json'}, catch_response=True, name="setupBlocks") as response:
       json_data = response.json()
       if "error" in json_data:
-        response.failure(json_data["error"]["message"])
-    
+        print(json_data["error"])
+        raise StopUser()
+      self.minimum_slot = json_data["result"]
+
+    # Fetch the epoch info to find slots/blocks for RCPcalls
+    with self.client.post('/', data=self.get_req_json("getEpochInfo"),  headers={'content-type': 'application/json'}, catch_response=True, name="setupBlocks") as response:
+      try:
+        json_data = response.json()
+        if "error" in json_data:
+          print(json_data["error"])
+          raise StopUser()
+          
+        # Get epoch info 
+        absolute_slot = json_data["result"]["absoluteSlot"]
+        slot_index = json_data["result"]["slotIndex"]
+        block_height = json_data["result"]["blockHeight"]
+        first_slot = absolute_slot - slot_index + 1
+        if self.minimum_slot > first_slot:
+          first_slot = self.minimum_slot
+
+        #  Find a random valid block
+        for i in range(100):
+          self.block = random.randint(first_slot, absolute_slot-1)
+          with self.client.post('/', data=self.get_req_json("getConfirmedBlock", [self.block]),  headers={'content-type': 'application/json'}, catch_response=True, name="setupBlocks") as response:
+            try: 
+              conf_block = response.json()
+              if "result" in conf_block and not "error" in conf_block:
+                break # wait until we find a valid block not slipped slot
+              elif(i == 99): # couldn't find a valid block 
+                raise StopUser()
+            except json.decoder.JSONDecodeError as e:
+              print(response.text)
+              raise StopUser()
+
+        # Pick a random block/slot range
+        self.start_block = random.randint(first_slot, absolute_slot-1)
+        self.end_block = random.randint(self.start_block+1, absolute_slot)
+        if self.end_block < self.start_block:
+          self.start_block = self.end_block-random.randint(0,10)    
+      except json.decoder.JSONDecodeError as e:
+        print(response.text)
+        raise StopUser()
+
+# A solana explorer type user
+class ExplorerUser(SolanaUser):
+  weight = 1
+
+  @task
+  def get_slot(self):
+    self.rpc("getSlot")
+
+  @task
+  def get_slot_leader(self):
+    self.rpc("getSlotLeader")
+
+  @task
+  def get_leader_schedule(self):
+    self.rpc("getLeaderSchedule")
+
+  @task
+  def get_cluster_nodes(self):
+    self.rpc("getClusterNodes")
+
+  @task
+  def get_epoch_schedule(self):
+    self.rpc("getEpochSchedule")
+
+  @task
+  def get_largest_accounts(self):
+    self.rpc("getLargestAccounts")
+
+  @task
+  def get_epoch_info(self):
+    self.rpc("getEpochInfo")
+
+  @task
+  def minimum_ledger_slot(self):
+    self.rpc("minimumLedgerSlot")
+
+# A generic traffic simulator user
+class TrafficSimulatorUser(SolanaUser):
+  weight = 10
+
   @task(52)
   def get_account_info(self):
     self.rpc("getAccountInfo", [self.wallet, {"encoding": "base58"}])
@@ -130,53 +233,3 @@ class TrafficSimulator(HttpUser):
   def get_fees(self):
     self.rpc("getFees")
 
-  def on_start(self):
-    # Randomly select values
-    self.stake_account = random.choice(stake_accounts)
-    self.wallet = random.choice(wallets)
-    self.wallets = random.sample(wallets,2)
-    self.confirmed_sigs = random.choice(confirmed_signature_address)
-    self.token_owner = random.choice(token_owners)
-    self.token_account = random.choice(token_accounts)
-    self.program_key = random.choice(program_keys)
-    self.transaction_signature = random.choice(transaction_signatures)
-    self.mint = random.choice(mints)
-   
-    # Get minimum stored ledger slot
-    with self.client.post('/', data=self.get_req_json("minimumLedgerSlot"),  headers={'content-type': 'application/json'}, catch_response=True, name="setupBlocks") as response:
-      json_data = response.json()
-      if "error" in json_data:
-        print(json_data["error"])
-        raise StopUser()
-      self.minimum_slot = json_data["result"]
-
-    # Fetch the epoch info to find slots/blocks for RCPcalls
-    with self.client.post('/', data=self.get_req_json("getEpochInfo"),  headers={'content-type': 'application/json'}, catch_response=True, name="setupBlocks") as response:
-      json_data = response.json()
-      if "error" in json_data:
-        print(json_data["error"])
-        raise StopUser()
-        
-      # Get epoch info 
-      absolute_slot = json_data["result"]["absoluteSlot"]
-      slot_index = json_data["result"]["slotIndex"]
-      block_height = json_data["result"]["blockHeight"]
-      first_slot = absolute_slot - slot_index + 1
-      if self.minimum_slot > first_slot:
-        first_slot = self.minimum_slot
-
-      #  Find a random valid block
-      for i in range(100):
-        self.block = random.randint(first_slot, absolute_slot-1)
-        with self.client.post('/', data=self.get_req_json("getConfirmedBlock", [self.block]),  headers={'content-type': 'application/json'}, catch_response=True, name="setupBlocks") as response:
-          conf_block = response.json()
-          if "result" in conf_block and not "error" in conf_block:
-            break # wait until we find a valid block not slipped slot
-          elif(i == 99): # couldn't find a valid block 
-            raise StopUser()
-
-      # Pick a random block/slot range
-      self.start_block = random.randint(first_slot, absolute_slot-1)
-      self.end_block = random.randint(self.start_block+1, absolute_slot)
-      if self.end_block < self.start_block:
-        self.start_block = self.end_block-random.randint(0,10)
